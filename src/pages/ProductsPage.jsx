da38a0d8bc785f6379b41c48a6e29b46f9b11982
Gustavo Almeida <gustavoalm1202@gmail.com>
2026-09-16 22:29:34 -0300

    A

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,17 +1,26 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 
 const ProductsPage = () => {
 
     const API_URL = import.meta.env.VITE_API_URL
+    const [products, setProducts] = useState([])
 
     useEffect(() =>{
         fetch(`${API_URL}products/category/electronics`)
         .then((results)=> results.json())
-        .then((data)=> console.log(data))
+        .then((data)=> setProducts(data))
     },[])
 
     return (
-        <h1>Página de Produtos</h1>
+        <section>
+        <h1>Eletrônicos</h1>
+        {products.map((currentProduct)=> (
+            <div>
+                <h2>{currentProduct.title}</h2>
+                <img src={currentProduct.image}/>
+            </div>
+        ))}
+        </section>
     )
 }
 
