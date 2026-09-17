f66061b1bab99c639e45ed61f0744bb2d2b13529
Gustavo Almeida <gustavoalm1202@gmail.com>
2026-09-16 22:29:26 -0300

    Ultimaaula

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,4 +1,15 @@
+import { useEffect } from "react"
+
 const ProductsPage = () => {
+
+    const API_URL = import.meta.env.VITE_API_URL
+
+    useEffect(() =>{
+        fetch(`${API_URL}products/category/electronics`)
+        .then((results)=> results.json())
+        .then((data)=> console.log(data))
+    },[])
+
     return (
         <h1>Página de Produtos</h1>
     )
